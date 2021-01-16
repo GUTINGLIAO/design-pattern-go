f730e7ddf4301b2fbcc8c042e48c983354c515ec
GUTINHLIAO <gutingliao@163.com>
2021-01-16 14:48:38 +0800

    完成建造者模式

--- builder/builder.go
+++ builder/builder.go
@@ -1,10 +1,71 @@
 package builder
 
 type User struct {
-	name string
-	age int
-	sex int
-	owner string
+	Name   string
+	Age    int
+	Sex    int
+	Height int
+	Weight int
+	Father string
+	Mother string
+	Son    string
 }
 
+func NewUserBuilder() *UserBuilder {
+	return &UserBuilder{
+		user: User{},
+	}
+}
 
+type UserBuilder struct {
+	user User
+}
+
+func (u *UserBuilder) Build() *User {
+
+	if u.user.Name == "" {
+		print("warning, name parameter is required")
+	}
+
+	return &u.user
+}
+
+func (u *UserBuilder) Name(name string) *UserBuilder {
+	u.user.Name = name
+	return u
+}
+
+func (u *UserBuilder) Age(age int) *UserBuilder {
+	u.user.Age = age
+	return u
+}
+
+func (u *UserBuilder) Sex(sex int) *UserBuilder {
+	u.user.Sex = sex
+	return u
+}
+
+func (u *UserBuilder) Height(height int) *UserBuilder {
+	u.user.Height = height
+	return u
+}
+
+func (u *UserBuilder) Weight(weight int) *UserBuilder {
+	u.user.Weight = weight
+	return u
+}
+
+func (u *UserBuilder) Father(father string) *UserBuilder {
+	u.user.Father = father
+	return u
+}
+
+func (u *UserBuilder) Mother(mother string) *UserBuilder {
+	u.user.Mother = mother
+	return u
+}
+
+func (u *UserBuilder) Son(son string) *UserBuilder {
+	u.user.Son = son
+	return u
+}
--- builder/builder_test.go
+++ builder/builder_test.go
@@ -1,1 +1,18 @@
 package builder_test
+
+import (
+	. "github.com/GUTINGLIAO/design-pattern-go/builder"
+	"testing"
+)
+
+func TestUserBuilder_Build(t *testing.T) {
+	userBuilder := NewUserBuilder()
+	userBuilder.
+		Name("Derek").
+		Father("Cloud").
+		Son("Huston").
+		Age(22).
+		Sex(0).
+		Height(20).
+		Build()
+}
