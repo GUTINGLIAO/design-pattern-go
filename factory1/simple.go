155702dccf71cce329927d58736cc77b0c3bb204
GUTINHLIAO <gutingliao@163.com>
2021-01-18 23:40:21 +0800

    修改文件

---
+++ factory1/simple.go
@@ -0,0 +1,54 @@
+/**
+factory包实现了工厂模式及其一系列变种
+
+工厂模式
+作用
+1. 解耦，调用方不需要考虑产品的生产过程，只需要传入相应的类型，也可以说是将创建实例的控制权交给了工厂
+2. 通过接口约定了产品需要实现的功能
+
+*/
+
+package factory1
+
+import (
+	"errors"
+	"fmt"
+)
+
+const (
+	PencilType = iota
+	PenType
+)
+
+type Product interface {
+	Detail()
+}
+
+type Pencil struct {
+	Width int
+	Depth int
+}
+
+func (p *Pencil) Detail() {
+	fmt.Printf("Depth:%v, Width:%v", p.Depth, p.Width)
+}
+
+type Pen struct {
+	Width  int
+	Length int
+}
+
+func (p *Pen) Detail() {
+	fmt.Printf("Length:%v, Width:%v", p.Length, p.Width)
+}
+
+func NewProduct(productType int) (Product, error) {
+	switch productType {
+	case PencilType:
+		return &Pencil{}, nil
+	case PenType:
+		return &Pen{}, nil
+	default:
+		return nil, errors.New("wrong type")
+	}
+}
---
+++ factory1/simple_test.go
@@ -0,0 +1,36 @@
+package factory1_test
+
+import (
+	. "github.com/GUTINGLIAO/design-pattern-go/factory1"
+	"reflect"
+	"testing"
+)
+
+func TestNewProductPositive(t *testing.T) {
+	inputs := []int{PenType, PencilType, 3}
+	expected := []reflect.Type{reflect.TypeOf(&Pen{}), reflect.TypeOf(&Pencil{})}
+	for i := 0; i < len(inputs); i++ {
+		p, err := NewProduct(inputs[i])
+		if err != nil {
+			t.Errorf("test failed, %v", err.Error())
+		}
+
+		if reflect.TypeOf(p) != expected[i] {
+			t.Errorf("test Failed, wrong type")
+		}
+	}
+}
+
+func TestNewProductNegative(t *testing.T) {
+	inputs := []int{3, 4, 5, 6}
+	expected := "wrong type"
+	for i := 0; i < len(inputs); i++ {
+		_, err := NewProduct(inputs[i])
+		if err == nil {
+			t.Fatal("test Failed, an error is expected")
+		}
+		if err.Error() != expected {
+			t.Fatal("test Failed, wrong err message")
+		}
+	}
+}
